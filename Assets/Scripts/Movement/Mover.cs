724156a96a49f34c345e3c8c7b824534a9d5694d
Anton Skatt <k8asnakeload@gmail.com>
2019-03-29 17:58:18 +0100

    sdefs

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Control;
+using RPG.Combat;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        [SerializeField] Transform target;
+
+        NavMeshAgent agent;
+
+        private void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            agent.destination = destination;
+            agent.isStopped = false;
+        }
+
+
+        public void Cancel()
+        {
+            agent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = agent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
+    }
+}
