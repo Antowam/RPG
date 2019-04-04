bdb2d894b1f7ce529c261305645f8ffa16a18320
Anton Skatt <k8asnakeload@gmail.com>
2019-04-04 17:29:26 +0200

    dwqd

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -5,6 +5,7 @@ using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -16,10 +17,14 @@ namespace RPG.Control
         [SerializeField] float waypointTolerance = 1f;
         [SerializeField] float waypointDwellTime = 3f;
 
+        [Range(0,1)]
+        [SerializeField] float patrolSpeedFraction = 0.2f;
+
         Fighter fighter;
         Health health;
         Mover mover;
         GameObject player;
+        NavMeshAgent agent;
 
         Vector3 guardPosition;
         float timeSinceLastSawPlayer = Mathf.Infinity;
@@ -31,6 +36,8 @@ namespace RPG.Control
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
             mover = GetComponent<Mover>();
+            agent = GetComponent<NavMeshAgent>();
+
             player = GameObject.FindWithTag("Player");
 
             guardPosition = transform.position;
@@ -78,7 +85,7 @@ namespace RPG.Control
             }
 
             if(timeSinceArrivedAtWaypoint > waypointDwellTime)
-                mover.StartMoveAction(nextPosition);
+                mover.StartMoveAction(nextPosition, patrolSpeedFraction);
         }
 
         private bool AtWaypoint()
